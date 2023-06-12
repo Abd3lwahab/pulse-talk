54e90f0c5c8a6f3951b1533c031209a9149e9320
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-06-13 02:24:09 +0300

    util for returning sorted chat id

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function getChatId(id1: string, id2: string) {
+  const sortedIds = [id1, id2].sort()
+  return `${sortedIds[0]}_${sortedIds[1]}`
+}
+
 export const getPusherChannelName = (key: string) => {
   return key.replace(/:/g, '-')
 }
